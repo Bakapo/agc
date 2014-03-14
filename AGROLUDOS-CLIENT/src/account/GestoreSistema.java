1360d97e3ec87eb559d389f6f40780c9f0ca6c6c
Bakapo <attbakapo@gmail.com>
2014-03-14 09:25:21 +0100

    Update GestoreSistema.java

--- AGROLUDOS-CLIENT/src/account/GestoreSistema.java
+++ AGROLUDOS-CLIENT/src/account/GestoreSistema.java
@@ -5,12 +5,12 @@ public class GestoreSistema extends Account {
 	private static GestoreSistema instance = null;
 	private GestoreSistema(String surname, String name, String email) {
 		super(surname, name, email);
-		// TODO Auto-generated constructor stub
 	}
 	
 	public static GestoreSistema getInstance() {
-        if (instance == null) 
-            instance = new GestoreSistema("Gestore", "Sistema", "agroludos@gmail.com");
+	        if (instance == null){
+	            instance = new GestoreSistema("Gestore", "Sistema", "agroludos@gmail.com");
+	        }
         return instance;
 	}
 }
